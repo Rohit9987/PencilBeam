3f8fe71e21c9f1b4bd4ae01b15965ed678c6a784
Rohit Inippully <rohit9987@gmail.com>
2023-11-23 16:43:40 +1100

    moving_on_to_terma

--- attenuation.cpp
+++ attenuation.cpp
@@ -11,40 +11,74 @@
    (MeV)      (cm2/g)     (cm2/g)
 */
 
+//initiate this as a class
+
+class Attenuation
+{
+	using Map = std::map<double, double>;
+	Map attenuation;
+
+public:
+	Attenuation(std::string material)
+	{
+		std::string line, s;
+		std::ifstream attenuation_file("./mass_attenuation/"+material);
+
+		std::string energy, mass_att, lin_att;
+		
+		while(attenuation_file >> energy >> mass_att >> lin_att)
+		{
+			try
+			{
+				attenuation[std::stod(energy)] = std::stod(lin_att);
+			}
+			catch(...) {}
+		}
+
+		attenuation_file.close();
+
+	}
+
+	~Attenuation() {}
+
+	double get_attenuation(double energy)		// material
+	{
+		double previous = -1;
+		Map::iterator it;
+		for(it = attenuation.begin(); it != attenuation.end(); it++)
+		{
+			if (energy == it->first)
+			{
+				return it->second;
+			}
+			else if (energy > it->first)
+			{
+				previous = it->first;
+			}
+			else
+			{
+				// interpolate
+				return interpolate(energy, it->first, it->second, previous, attenuation[previous]);
+			}
+		}
+
+		return 0;
+	}
+
+private:
+	double interpolate(double x, double x2, double y2, double x1, double y1)
+	{
+
+		double slope = (y2-y1)/(x2-x1);
+		double val = y1 + slope * (x-x1);
+		return val;
+	}
+};
+
 int main()
 {
-	std::string line, s;
-	std::ifstream attenuation_file("./mass_attenuation/water");
+	Attenuation att1("water");
+	std::cout << att1.get_attenuation(3.5);
 
-/*
-	while(getline(attenuation_file, line))
-	{
-		std::cout << line << "\n";
-	}
-
-*/
-
-	//std::map<std::string, std::string> attenuation;
-	std::map<double, double> attenuation;
-	std::string energy, mass_att, lin_att;
-
-	while(attenuation_file >> energy >> mass_att >> lin_att)
-	{
-		try
-		{
-
-		attenuation[std::stod(energy)] = std::stod(lin_att);
-		}
-		catch(...)
-		{}
-	}
-
-	attenuation_file.close();
-
-
-	for(auto s: attenuation)
-	{
-		std::cout << s.first << ": " << s.second << '\n';
-	}
+	return 0;
 }
-	
--- terma.cpp
+++ terma.cpp
@@ -17,3 +17,42 @@ https://www.nist.gov/pml/x-ray-mass-attenuation-coefficients
 
 */
 
+
+#define SSD 100.0
+
+class TERMA
+{
+public:
+	TERMA(double phi, double distance): _phi{phi}, _distance{distance}
+	{
+		std::cout << invsq(200);	
+
+	}
+private:
+	double _phi, _distance;
+	
+	double invsq(distance)
+	{
+		return distance * distance / (SSD * SSD);
+	}
+
+};
+
+
+int main()
+{
+	double phi = 1.0;
+
+	double distance = 100;
+
+	
+	return 0;
+
+}
+
+
+
+
+	
+
+
